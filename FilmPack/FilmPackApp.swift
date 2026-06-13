35d93d57b05937e2d61d9f3af488d85ae970f9a5
Aaron Villalobos <avilla4537@gmail.com>
2026-06-13 10:36:32 -0700

    Initial Commit

---
+++ FilmPack/FilmPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FilmPackApp.swift
+//  FilmPack
+//
+//  Created by Aaron Villalobos on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct FilmPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
